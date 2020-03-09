642c0d90a2bfcb62b55668a754ef3040be93b8a9
waljqiang <waljqiang@163.com>
2020-03-09 18:31:16 +0800

    12

--- docs/js/common.js
+++ docs/js/common.js
@@ -2,6 +2,9 @@ function show(obj){
 	$(obj).slideToggle();
 }
 
-function go(url){
-	window.location.href = url;
+function showParent(obj, number){
+	number = number-1;
+	var obj = $(obj,parent.document);
+	obj.css("display","block");
+	obj.find("li:eq("+number+")").find("a:eq("+number+")").addClass("selecta");
 }
